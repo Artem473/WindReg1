b1bb3065bddf1c97d4c7c7297f8e9589ac7d6f39
Max <gsx401@yandex.ru>
2020-10-21 12:37:58 +0300

    git remote add origin https://github.com/Artem473/-.git git branch -M main git push -u origin main

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,42 +2,65 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
-    String name = "";
-    String surname = "";
-    String patro = "";
-    String nameFile = "";
-    String pyth = "";
+
+    String extend = "";
+    String course = "";
+
+    @FXML
+    private TextField nameFile;
+    @FXML
+    private TextField pathFile;
+    @FXML
+    private TextField surname;
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField patron;
+    @FXML
+    private Label status;
 
     Model model = new Model();
-
-    @FXML
-    private TextField text;
-
-    @FXML
-    private Label label;
 
     public Controller() {
     }
 
     @FXML
-    public void pressCheckBox(){
-
+    public void pressCheckBox(ActionEvent event) {
+        course += " " + ((CheckBox) event.getSource()).getText();
     }
-    @FXML
-    public void pressRadio(){
 
-    }
     @FXML
-    public void pressButton(ActionEvent event){
-        String str = text.getText();
-        if (!str.isEmpty()){
-            label.setText("Статус: Вы зарегистрировались♥");
-        } else {
-            label.setText("Статус: Заполните анкету☻");
+    public void pressRadio(ActionEvent event) {
+        extend = ((RadioButton) event.getSource()).getText();
+    }
+
+    @FXML
+    public void pressButton(ActionEvent event) throws IOException {
+        model.createFile(name.getText(), surname.getText(), patron.getText(), nameFile.getText(), pathFile.getText(), extend, course);
+
+        status.setText(name.getText() + "," + " Вы успешно зарегистрировались");
+    }
+
+    @FXML
+    public void selectPath(ActionEvent event) throws IOException {
+        model.openPath(pathFile);
         }
+    @FXML
+    public void selectFile(ActionEvent event) throws IOException {
+        model.openFile(pathFile.getText(),nameFile.getText(),extend);
+
+
     }
+
 }
